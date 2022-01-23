7158ad26c04d9fa59578ecc256ece06609b23d58
Himanshu Srivastava <84365673+himanshuSri24@users.noreply.github.com>
2022-01-23 15:37:16 +0530

    Time: 2 ms (98.41%), Space: 39.3 MB (58.28%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
@@ -1,40 +1,19 @@
 class Solution {
     public String removeOuterParentheses(String s) {
-        int openCount = 0, closeCount = 0;
-        List<String> list = new ArrayList<String>();
-        StringBuilder sb = new StringBuilder("");
-        for(char ch : s.toCharArray())
+        StringBuilder str = new StringBuilder();
+        int count =0;
+        char sin[] = s.toCharArray();
+        for(char c : sin)
         {
-            if(ch == '(')
-            {
-                if(openCount == 0 || openCount == closeCount);
-                else
-                    sb.append(ch+"");
-                openCount ++;
-            }
-            else if(ch == ')')
-            {
-                if(closeCount == openCount);
-                
-                else
-                    sb.append(ch+"");
-                
-                closeCount++;
-                if(closeCount == openCount)
-                {
-                    list.add(sb.toString());
-                    System.out.println(sb);
-                    sb.setLength(0);
-                }
-            }
+        
+            if(c == ')')
+                count--;
+            if(count > 0)
+                str.append(c);
+            if(c == '(')
+                count++;
         }
-        System.out.println(sb);
-        sb.setLength(0);
-        for(String word : list)
-        {
-            System.out.println(word);
-            sb.append(word.substring(0,word.length()-1));
-        }
-        return sb.toString();
+    
+        return str.toString();
     }
 }
