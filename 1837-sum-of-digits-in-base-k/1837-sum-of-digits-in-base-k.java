ca0a9f5b50c38651d7879ff1c85c145ba7ee38dd
Himanshu Srivastava <84365673+himanshuSri24@users.noreply.github.com>
2022-01-25 19:26:23 +0530

    Time: 0 ms (100.00%), Space: 37.5 MB (33.33%) - LeetHub

--- 1837-sum-of-digits-in-base-k/1837-sum-of-digits-in-base-k.java
+++ 1837-sum-of-digits-in-base-k/1837-sum-of-digits-in-base-k.java
@@ -1,17 +1,12 @@
 class Solution {
 
-    public int sumOfDig(int n)
-    {
-        int sum = 0;
+    public int sumBase(int n, int k) {
+        int sum=0;
         while(n > 0)
         {
-            sum += n%10;
-            n /= 10;
+            sum += n % k;
+            n /= k;
         }
         return sum;
-    }
-    
-    public int sumBase(int n, int k) {
-        return sumOfDig(Integer.valueOf(Integer.toString(Integer.parseInt(Integer.toString(n), 10), k)));
     }
 }
