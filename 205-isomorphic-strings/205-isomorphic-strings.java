e6da377af56984dffc53ed396ed597224bd3dbe4
Himanshu Srivastava <84365673+himanshuSri24@users.noreply.github.com>
2022-01-28 11:27:41 +0530

    Time: 65 ms (5.09%), Space: 45.9 MB (5.02%) - LeetHub

---
+++ 205-isomorphic-strings/205-isomorphic-strings.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        //Make a HashMap which maps Char -> Char for O(1) access
+        //if an element doesn't exist, add it
+        //create a new stringBuilder with the values of hashMap
+        //check if equal
+        Map<Character, Character> map = new HashMap<>();
+        if(s.length() != t.length())
+            return false;
+        int len = s.length();
+        StringBuilder ans = new StringBuilder("");
+        for(int i = 0; i < len; i ++)
+        {
+            char cs = s.charAt(i);
+            char ct = t.charAt(i);
+            if(!map.containsValue(ct))
+                map.putIfAbsent(cs, ct);
+            ans.append(map.get(cs));
+            // System.out.println(ans);
+        }
+        return ans.toString().equals(t);
+    }
+}
